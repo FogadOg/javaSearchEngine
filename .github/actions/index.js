b051c895b999a9c51cc746e06740dfff941fd5b0
FogadOg <115077292+FogadOg@users.noreply.github.com>
2023-11-15 14:58:00 +0100

    testing the github actions

--- .github/actions/index.js
+++ .github/actions/index.js
@@ -1,1 +1,5 @@
-console.log("hello from github workflow, you made a push request");
+const param1 = process.env.INPUT_PARAM1;
+const param2 = process.env.INPUT_PARAM2;
+
+console.log(`Param 1: ${param1}`);
+console.log(`Param 2: ${param2}`);
