b728e82e93f64e81f52e04df5f84a81dd646a5f6
FogadOg <115077292+FogadOg@users.noreply.github.com>
2023-11-15 15:24:04 +0100

    edited the index.js

--- .github/actions/index.js
+++ .github/actions/index.js
@@ -1,18 +1,38 @@
+
+
 const fs = require('fs');
 
-fs.readFile('./.github/actions/myText.txt', 'utf8', (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-
-  const modifiedData = data.replace(/color/g, 'colour');
-
-  fs.writeFile('./.github/actions/myText.txt', modifiedData, 'utf8', (err) => {
+function replaceTextInFile(filePath, oldText, newText) {
+  fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
       console.error(err);
       return;
     }
-    console.log('File updated successfully.');
+
+    const modifiedData = data.replace(new RegExp(oldText, 'g'), newText);
+
+    fs.writeFile(filePath, modifiedData, 'utf8', (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log(`Text '${oldText}' replaced with '${newText}' in file '${filePath}'.`);
+    });
   });
-});
+}
+
+module.exports = replaceTextInFile;
+
+
+
+
+
+
+
+const replaceTextInFile = require('./replaceText');
+
+const filePath = './.github/actions/myText.txt'; // Replace this with your file path
+const oldText = 'color';
+const newText = 'colour';
+
+replaceTextInFile(filePath, oldText, newText);
