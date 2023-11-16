001bcd57d217adb62259f2dca45e68d84cb1eb53
FogadOg <fogadm06@gmail.com>
2023-11-16 20:31:05 +0100

    testing new solutin in the pushRequest.yml

--- script.py
+++ script.py
@@ -10,6 +10,9 @@ updated_content = file_content.replace(old_word, new_word)
 print("updated_content : ",updated_content)
 
 with open("file2.txt", 'w') as file:
-    file.write(updated_content)
+    file.write("This is a sample text that will be written to the file.\n")
+    file.write("Writing to a text file in Python is straightforward!\n")
+    # You can write multiple lines by calling file.write() multiple times
+
 
 print(f"Word '{old_word}' replaced with '{new_word}' in {file_path}")
