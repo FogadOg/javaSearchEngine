ebdd8aedc2fd153d2f5f160672feba6c1ca8c587
FogadOg <fogadm06@gmail.com>
2023-11-16 20:28:51 +0100

    testing new solutin in the pushRequest.yml

--- script.py
+++ script.py
@@ -9,7 +9,7 @@ updated_content = file_content.replace(old_word, new_word)
 
 print("updated_content : ",updated_content)
 
-with open(file_path, 'w') as file:
+with open("file2.txt", 'w') as file:
     file.write(updated_content)
 
 print(f"Word '{old_word}' replaced with '{new_word}' in {file_path}")
