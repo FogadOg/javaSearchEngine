dfcaa91c247726b46c1a1ca1687196d3e8bc0407
FogadOg <fogadm06@gmail.com>
2023-11-17 18:30:16 +0100

    test

--- main.py
+++ main.py
@@ -2,21 +2,22 @@ import sys
 
 oldWord=sys.argv[1]
 newWord=sys.argv[2]
+filePath=sys.argv[3]
 
 
-def overwriteFile(oldWords,newWord):
+def overwriteFile(oldWords,newWord,filePath):
 
     oldWords=oldWords.split(",")
 
-    read=open("config.yml","r")
+    read=open(filePath,"r")
     text=read.read()
 
     replaceAction = ' '.join([newWord if idx in oldWords else idx for idx in text.split()])
     read.close()
 
-    write=open("config.yml","w")
+    write=open(filePath,"w")
     write.write(replaceAction)
     write.close()
 
 
-overwriteFile(oldWord,newWord)
+overwriteFile(oldWord,newWord,filePath)
