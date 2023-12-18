12bff68a24068d7143899c4737e90c1f5a06f87a
FogadOg <fogadm06@gmail.com>
2023-12-18 19:52:11 +0100

    finished the image search component

--- src/main/java/com/example/searchengine/components/search/ImageSearch.java
+++ src/main/java/com/example/searchengine/components/search/ImageSearch.java
@@ -6,6 +6,9 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 import java.io.FileReader;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class ImageSearch extends Search{
 
@@ -37,21 +40,50 @@ public class ImageSearch extends Search{
                 JSONArray imagesArray = (JSONArray) website.get("images");
 
 
-                for(Object image: imagesArray){
-                    Integer rating=rateImage();
-                    System.out.println("image: "+image);
+                for(Object imageObject: imagesArray){
+                    JSONObject jsonImageObject = (JSONObject) imageObject;
+                    Integer rating=rateImage(jsonImageObject, stemmedSearchTrem);
+                    jsonImageObject.put("rating",rating);
+
+                    proccessedImages.add(jsonImageObject);
+
                 }
 
             }
-
+            sortArray(proccessedImages);
         }catch (Exception e){
             e.printStackTrace();
         }
         return proccessedImages;
     }
 
-    private Integer rateImage(){
-        Integer initialRating=0;
+    private void sortArray(JSONArray JsonArray){
+
+        Collections.sort(JsonArray, (o1, o2) -> {
+            JSONObject obj1 = (JSONObject) o1;
+            JSONObject obj2 = (JSONObject) o2;
+
+            Integer rating1 = Integer.parseInt(obj1.get("rating").toString());
+            Integer rating2 = Integer.parseInt(obj2.get("rating").toString());
+
+
+            return rating2.compareTo(rating1);
+        });
+    }
+
+    private Integer rateImage(JSONObject imageObject,String stemmedSearchTrem){
+        Integer initialRating=10;
+
+        String imageAlt=imageObject.get("alt").toString();
+        if(imageAlt.trim().isEmpty()){
+            initialRating-=5;
+        }
+
+        String stemmedAltAttribute=stemmer.stemString(imageAlt);
+        int[] nGrams={1,2,3,4,5};
+        Integer nGramPoints=checkForNGramRepet(nGrams,stemmedSearchTrem,stemmedAltAttribute);
+
+        initialRating+=nGramPoints;
 
 
         return initialRating;
