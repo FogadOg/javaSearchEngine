598259de84e2e937ece98e9cbce0d104f1601e34
FogadOg <fogadm06@gmail.com>
2024-01-01 18:50:35 +0100

    adding a service component with some methods

---
+++ src/main/java/com/example/searchengine/components/WebsiteService.java
@@ -0,0 +1,19 @@
+package com.example.searchengine.components;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WebsiteService {
+    public JSONObject replaceField(JSONObject jsonObject,String field, String newString){
+        return jsonObject.put(field, newString);
+    }
+
+    public JSONObject replaceField(JSONObject jsonObject,String field, JSONObject newObject){
+        return jsonObject.put(field, newObject);
+    }
+    public JSONObject replaceField(JSONObject jsonObject, String field, JSONArray newArray){
+        return jsonObject.put(field, newArray);
+    }
+}
