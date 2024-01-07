c16a67e4472c7faf1ec1e976d01716fc4365e55d
FogadOg <fogadm06@gmail.com>
2024-01-07 14:39:21 +0100

    making the component functional

--- src/main/java/com/example/searchengine/components/search/ImageSearch.java
+++ src/main/java/com/example/searchengine/components/search/ImageSearch.java
@@ -1,6 +1,32 @@
 package com.example.searchengine.components.search;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public class ImageSearch extends Search{
+
+    private final JSONArray websites;
+
+    public ImageSearch(JSONArray websiteData){
+        this.websites=websiteData;
+    }
+
+    public JSONArray search(String searchTerm){
+        JSONArray relevantImages=new JSONArray();
+
+        for(int index=0;index<websites.length(); index++){
+            JSONObject website= (JSONObject) websites.get(index);
+            JSONArray images = (JSONArray) website.get("images");
+
+            for(Object image: images){
+                relevantImages.put(image);
+            }
+
+        }
+
+
+        return relevantImages;
+    }
 
 
 }
