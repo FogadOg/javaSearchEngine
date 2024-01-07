c13bc137f9196e17fefd72953fdca2770a1ce364
FogadOg <fogadm06@gmail.com>
2024-01-07 19:14:18 +0100

    implementing tf-idf for image search

--- src/main/java/com/example/searchengine/components/search/ImageSearch.java
+++ src/main/java/com/example/searchengine/components/search/ImageSearch.java
@@ -6,12 +6,15 @@ import org.json.JSONObject;
 public class ImageSearch extends Search{
 
     private final JSONArray websites;
+    private final String searchTerm;
 
-    public ImageSearch(JSONArray websiteData){
+    public ImageSearch(JSONArray websiteData, String searchTerm){
+
         this.websites=websiteData;
+        this.searchTerm=searchTerm;
     }
 
-    public JSONArray search(String searchTerm){
+    public JSONArray search(){
         JSONArray relevantImages=new JSONArray();
 
         for(int index=0;index<websites.length(); index++){
@@ -19,13 +22,20 @@ public class ImageSearch extends Search{
             JSONArray images = (JSONArray) website.get("images");
 
             for(Object image: images){
-                relevantImages.put(image);
+                JSONObject imageObject= (JSONObject) image;
+                String imageAlt = imageObject.get("alt").toString();
+                if(isImageRelevant(imageAlt, .1)){
+                    relevantImages.put(image);
+                }
             }
-
         }
 
 
         return relevantImages;
+    }
+
+    private Boolean isImageRelevant(String altText, Double threshHold){
+        return calculateTextsSimilarity(searchTerm, altText) >= threshHold;
     }
 
 
