aa31c48dc3fcd17f65cbba61e118e8c4e508787a
FogadOg <fogadm06@gmail.com>
2024-01-13 15:17:59 +0100

    adding method to count backlinks

--- src/main/java/com/example/searchengine/components/crawler/CrawlerService.java
+++ src/main/java/com/example/searchengine/components/crawler/CrawlerService.java
@@ -4,9 +4,14 @@ package com.example.searchengine.components.crawler;
 
 import com.example.searchengine.components.services.JsonFileService;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 
 @Service
@@ -27,7 +32,25 @@ public class CrawlerService {
         return false;
     }
 
-    public static void main(String[] args){
+    public void incrementBackLinkCount(String url){
+        JSONObject jsonObject=jsonFileService.readJsonFileObject("backLinkCount.Json");
+        Integer idfCount=getBackLinkCount(url);
+        jsonObject.put(url, idfCount+1);
 
+        JsonFileService.writeJsonToFileObject(jsonObject, "backLinkCount.json");
+
+    }
+    private Integer getBackLinkCount(String term){
+        JSONObject jsonObject=jsonFileService.readJsonFileObject("backLinkCount.Json");
+        try {
+            return jsonObject.getInt(term);
+        } catch (JSONException e) {
+            return 0;
+        }
+    }
+
+    public void addPageToJson(String url){
+        UrlDataJsonObject urlDataJsonObject= new UrlDataJsonObject("data.json", url);
+        urlDataJsonObject.addUrlDataToJsonFile();
     }
 }
